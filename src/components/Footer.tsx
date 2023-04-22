7481ebf0f0503140fc84452d4915a8f711bf7d61
Conrad Hunter <conrad.hunter10@gmail.com>
2023-04-22 13:21:31 +1000

    initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+
+export const Footer: FunctionComponent = () => {
+  return (
+    <footer>
+      <h1>HELLO</h1>
+      <h1>This is a footer</h1>
+    </footer>
+  );
+};
