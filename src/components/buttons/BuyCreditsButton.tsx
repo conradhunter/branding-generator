4dea995f236dccbb1b1f5de77c0869eba4fdded2
Conrad Hunter <conrad.hunter10@gmail.com>
2023-04-22 22:44:16 +1000

    create dynamic display for navbar links & buttons;

---
+++ src/components/buttons/BuyCreditsButton.tsx
@@ -0,0 +1,9 @@
+const BuyCreditsButton = () => {
+  return (
+    <button className='rounded-md border border-violet-400 px-3 py-2 text-sm font-medium uppercase text-white shadow-lg duration-150 hover:bg-violet-400'>
+      Buy Credits
+    </button>
+  );
+};
+
+export default BuyCreditsButton;
