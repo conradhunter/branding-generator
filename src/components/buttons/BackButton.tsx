6bbacfa5784ec9a470029c69099ea0e7ce549a36
Conrad Hunter <conrad.hunter10@gmail.com>
2023-04-24 21:30:16 +1000

    .

--- src/components/buttons/BackButton.tsx
+++ src/components/buttons/BackButton.tsx
@@ -1,9 +1,17 @@
-import Link from 'next/link';
+'use client';
+
+import BackArrow from '../icons/BackArrow';
+import { useRouter } from 'next/navigation';
 
 const BackButton = () => {
+  const router = useRouter();
+
   return (
-    <button className='absolute left-10 top-10 rounded-md bg-violet-400 px-3 py-2 text-sm text-white shadow-lg duration-150 hover:bg-violet-500'>
-      <Link href='/'>Go Back</Link>
+    <button
+      onClick={() => router.back()}
+      className='absolute left-10 top-10 flex items-center rounded-md bg-violet-400 px-3 py-2 text-sm text-white shadow-lg duration-150 hover:bg-violet-500'
+    >
+      <BackArrow /> Go Back
     </button>
   );
 };
---
+++ src/components/icons/BackArrow.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+const BackArrow = () => {
+  return (
+    <>
+      <ArrowBackIcon className='mr-1' />
+    </>
+  );
+};
+
+export default BackArrow;
