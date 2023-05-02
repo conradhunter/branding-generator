a55455cbb72a7bd094b8d8249ae009d61b8d3310
Conrad Hunter <conrad.hunter10@gmail.com>
2023-05-02 18:56:25 +1000

    Fixed error of environment variables returning undefined

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  reactStrictMode: true,
+  env: {
+    OPEN_AI_API_KEY: process.env.DALL_E_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
--- src/app/api/openAI/server.ts
+++ src/app/api/openAI/server.ts
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey: process.env.DALL_E_API_KEY!,
+  apiKey: process.env.OPEN_AI_API_KEY!,
   organization: 'org-pPpZQP6QHreKXJuAzPxRizq8'
 });
 
@@ -9,8 +9,7 @@ export async function generateLogo() {
   const openai = new OpenAIApi(configuration);
 
   const response = await openai.createImage({
-    //prompt: 'High-end premium modern logo of the letters "CONRAD", featured on 99designs',
-    prompt: 'a cartoon chicken with a pink tye on',
+    prompt: 'High-end premium modern logo of the letters "KATHY", featured on 99designs',
     n: 1,
     size: '256x256',
   });
