1b49d258492abc43a84e89ec357fd2fa452af4e7
Conrad Hunter <conrad.hunter10@gmail.com>
2023-05-08 08:29:47 +1000

    remove upload-thing code

--- src/app/api/uploadThing/route.ts
+++
@@ -1,7 +0,0 @@
-import { ourFileRouter } from "./core";
-import { createNextRouteHandler } from "uploadthing/server";
- 
-// Export routes for Next App Router
-export const { POST } = createNextRouteHandler({
-  router: ourFileRouter,
-});
