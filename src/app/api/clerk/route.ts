32331081320b9f269e6c8a615b66312c4c6c6e1a
Conrad Hunter <conrad.hunter10@gmail.com>
2023-05-10 18:18:07 +1000

    .

--- src/app/api/clerk/route.ts
+++ src/app/api/clerk/route.ts
@@ -1,7 +1,7 @@
 import { clerkClient, currentUser } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-export async function PATCH(reponse: Response) {
+export async function PATCH(request: Request) {
   const user = await currentUser();
 
   const updatedUser = await clerkClient.users.updateUserMetadata(user?.id!, {
