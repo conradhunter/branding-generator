f8353d03ce00ccae947a2cdeebd0bb71a2ebdb72
Conrad Hunter <conrad.hunter10@gmail.com>
2023-05-30 18:55:59 +1000

    chore: add loading UI file to all necessary routes

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,22 +1,8 @@
-import Image from 'next/image';
 import React from 'react';
-import loading2 from '../../public/loading_undraw_2.svg';
+import LoadingUI from '~/components/LoadingUI';
 
 const loading = () => {
-  return (
-    <section className='flex h-[calc(100vh-72px)] items-center justify-center bg-slate-800'>
-      <h1 className='text-3xl text-white'>Loading...</h1>
-      <div className='container flex items-center justify-center'>
-        <Image
-          src={loading2}
-          alt='Loading...'
-          priority={true}
-          width={300}
-          height={300}
-        />
-      </div>
-    </section>
-  );
+  return <LoadingUI />;
 };
 
 export default loading;
---
+++ src/components/LoadingUI.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import React from 'react';
+import loading2 from '../../public/loading_undraw_2.svg';
+
+const LoadingUI = () => {
+  return (
+    <section className='flex h-[calc(100vh-72px)] items-center justify-center bg-slate-800'>
+      <h1 className='text-3xl text-white'>Loading...</h1>
+      <div className='container flex items-center justify-center'>
+        <Image
+          src={loading2}
+          alt='Loading...'
+          priority={true}
+          width={300}
+          height={300}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default LoadingUI;
